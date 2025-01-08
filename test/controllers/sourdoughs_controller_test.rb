dd9326285701c13ed018ec8b53332f29e0dcf3fd
Victoria Coquet <pupicoquet@hotmail.com>
2025-01-08 17:56:00 +0000

    css-components

---
+++ test/controllers/sourdoughs_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SourdoughsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/starters_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StartersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
